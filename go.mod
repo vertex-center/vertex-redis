e673cf347e2fdaadbf5be8890df9bc70f3190a58
Quentin Guidée <quentin.guidee@gmail.com>
2023-03-27 20:33:03 -0400

    Add service.json and port
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/quentinguidee/redis-service
 
 go 1.19
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/joho/godotenv v1.5.1
+	github.com/vertex-center/vertex-core-golang v0.0.0-20230327174744-2ac235efcc9f
+)
+
+require (
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- init.go
+++ init.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+	"github.com/vertex-center/vertex-core-golang/console"
 )
 
+var logger = console.New("vertex-redis")
+
+type Environment struct {
+	Port string `env:"PORT" envDefault:"6379"`
+}
+
+var environment Environment
+
 func main() {
-	command := exec.Command("redis-server")
+	loadEnv()
+
+	command := exec.Command("redis-server", "--port", environment.Port)
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	command.Stdin = os.Stdin
@@ -14,5 +29,17 @@ func main() {
 	err := command.Run()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error(fmt.Errorf("error loading .env file: %v", err))
+	}
+
+	err = env.Parse(&environment)
+	if err != nil {
+		logger.Error(fmt.Errorf("failed to parse .env to Config: %v", err))
 	}
 }
